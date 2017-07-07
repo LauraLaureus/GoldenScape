9661e88e3c6b2acb4079f11c56fc9f63aaf53b89
Laura <lauradelpinodiaz94@gmail.com>
2017-07-07 11:50:50 +0200

    Base commit

---
+++ GoldenGaze/GoldenGaze/Assets/_Scripts/Applicator.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using System;
+
+public class Applicator : MonoBehaviour {
+
+	public delegate void TerrainGenerated(Door d);
+	public static event TerrainGenerated OnTerrainGenerated;
+
+
+
+	private Terrain terrain;
+	public float factor;
+	public float wallheight;
+
+
+	void OnEnable(){
+		Dungeon.OnMapCreated += apply;
+	}
+
+	protected void apply(MapCell[,] map,Door door){
+
+		terrain = this.gameObject.GetComponent<Terrain> ();
+		//Debug.Log ("Dimensiones del mapa de alturas:" + terrain.terrainData.heightmapWidth + "x" + terrain.terrainData.heightmapHeight);
+		float[,] heights = terrain.terrainData.GetHeights (0, 0, terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapWidth);
+		applyCellMap (heights,map);
+		callPlayerCreator (door);
+	}
+
+	void applyCellMap(float[,] heights , MapCell[,]map){
+
+		for (int i = 0; i < heights.GetLength(0); i++) {
+			for(int j = 0; j < heights.GetLength(1); j++){
+				heights[i,j] = getSuitableHeightFromMap(map,i,j, heights);
+			}
+		}
+		heights = generateDistanceTransform(heights);
+		terrain.terrainData.SetHeights (0, 0, heights);
+	}
+
+	float getSuitableHeightFromMap(MapCell[,] map, int indexX, int indexY, float[,] heights){
+
+		int indexXmap = Mathf.Clamp ((int)(indexX / factor), 0, map.GetLength(0)-1);
+		int indexYmap = Mathf.Clamp ((int)(indexY / factor), 0, map.GetLength(1)-1);
+		if (map[indexXmap, indexYmap].cellKind == MapCell.CellKind.WALKABLE && !map[indexXmap, indexYmap].isBorder)
+			return 0f;
+		else
+			return wallheight;
+	}
+
+	private float[,] generateDistanceTransform(float[,] heights)
+	{
+		for (int i = 1; i < heights.GetLength(0); i++)
+		{
+			for (int j = 1; j < heights.GetLength(1); j++)
+			{
+
+				if (heights[j, i] == 0.0f) continue;
+				float min = wallheight;
+
+				for (int x = j - 1; x <= j + 1; x++)
+				{
+					if ((x >= 0) && (x < heights.GetLength(1)))
+					{
+						if (heights[x, i - 1] < min)
+							min = heights[x, i - 1];
+					}
+				}
+				if (heights[j - 1, i] < min)
+					min = heights[j - 1, i];
+				if (min < wallheight)
+					heights[j, i] = min + wallheight * 2 / factor;
+
+			}
+		}
+
+		for (int i = heights.GetLength(0) - 2; i >= 0; i--)
+		{
+			for (int j = heights.GetLength(1) - 2; j >= 0; j--)
+			{
+				if (heights[j, i] == 0) continue;
+
+				float min = heights[j, i];
+
+				for (int x = j - 1; x <= j + 1; x++)
+				{
+					if ((x >= 0) && (x < heights.GetLength(1)))
+					{
+						if (heights[x, i + 1] < min)
+							min = heights[x, i + 1];
+					}
+				}
+
+				if (heights[j + 1, i] < min)
+					min = heights[j + 1, i];
+
+
+				if (min < heights[j, i])
+					heights[j, i] = min + wallheight * 2 / factor;
+
+			}
+		}
+		return heights;
+	}
+
+	void callPlayerCreator(Door d){
+		Debug.Log ("Puerta:" + d.x_t + " " + d.y_t);
+		d.x_t *= terrain.terrainData.heightmapWidth;
+		d.y_t *= terrain.terrainData.heightmapHeight;
+		Debug.Log ("Puerta:" + d.x_t + " " + d.y_t);
+		if (OnTerrainGenerated != null)
+			OnTerrainGenerated (d);
+	}
+
+
+	void OnDisable(){
+		Dungeon.OnMapCreated -= apply;
+
+	}
+
+}
---
+++ GoldenGaze/GoldenGaze/Assets/_Scripts/Door.cs
@@ -0,0 +1,49 @@
+﻿public class Door {
+
+	public int x, y;
+	public int zoneFrom;
+	public int zoneTo;
+	public int doorDirection; // 0: horizontal, 1 vertical
+	MapCell [,] map;
+	public float x_t, y_t;
+
+	public Door (int x, int y, int zoneFrom, MapCell[,] map) {
+		this.x = x;
+		this.y = y;
+		this.zoneFrom = zoneFrom;
+		this.map = map;
+		zoneTo = 0;
+		doorDirection = -1;
+	}
+
+	public Door(){
+	}
+
+	public void translateInto(int width,int height){
+		x_t = ((float)x) / ((float)width);
+		y_t = ((float)y) / ((float)height);
+	}
+
+	public void updateDoorDirection () {
+		this.doorDirection = (map[x - 1, y].cellKind == MapCell.CellKind.WALKABLE) ? 0 : 1;
+	}
+
+	public void updateDoorZones () {
+		if (doorDirection == 0)
+			this.zoneTo = (map[x - 1, y].zoneID == zoneFrom) ? map[x + 1, y].zoneID : map[x - 1, y].zoneID;
+		else
+			this.zoneTo = (map[x, y - 1].zoneID == zoneFrom) ? map[x, y + 1].zoneID : map[x, y - 1].zoneID;
+	}
+
+	public bool compareWith(Door doorComp)
+	{
+		if ((doorComp.x == x) && (doorComp.y == y)) return true;
+		return false;
+	}
+
+	public bool Equals(Door doorComp)
+	{
+		if ((doorComp.x == x) && (doorComp.y == y)) return true;
+		return false;
+	}
+}
